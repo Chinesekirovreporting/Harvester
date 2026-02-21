58f12aa04a96ddb05950d7ab283eb4fafa18c4e5
泉水指挥官 <1067535354@qq.com>
2026-02-21 14:32:27 +0800

    接口暂存

--- trunk/client/project/Harvester/assets/scripts/core_fight/core_skill/ISkillVo.ts
+++ trunk/client/project/Harvester/assets/scripts/core_fight/core_skill/ISkillVo.ts
@@ -1,4 +1,4 @@
 // 抽象技能数据
 export interface ISkillVo {
-    public distance:numnber;    // 技能施法距离
+    distance:number;    // 技能施法距离
 }
--- trunk/client/project/Harvester/assets/scripts/core_fight/core_skill/SkillVo.ts
+++ trunk/client/project/Harvester/assets/scripts/core_fight/core_skill/SkillVo.ts
@@ -1,4 +1,7 @@
+import { ISkillVo } from "./ISkillVo";
+
 export class SkillVo implements ISkillVo {
+    public distance: number;
     constructor() {
 
     }
