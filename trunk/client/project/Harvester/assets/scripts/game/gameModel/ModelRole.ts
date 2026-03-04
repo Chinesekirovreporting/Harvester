bac103f4f054d0917509668d7d868836b63d5c6e
泉水指挥官 <1067535354@qq.com>
2026-03-05 03:32:02 +0800

    角色数据构建

--- trunk/client/project/Harvester/assets/scripts/game/gameModel/ModelRole.ts
+++ trunk/client/project/Harvester/assets/scripts/game/gameModel/ModelRole.ts
@@ -1,8 +1,22 @@
 import { App } from "../../framework/managers/App";
+import { Dictionary } from "../../framework/utils/Dictionary";
 import { AbstractModel } from "./AbstractModel";
 import { StorageKeys } from "./data/StorageKeys";
+import { HeroBookCFG } from "./table/tableClass/HeroBookCFG";
 
 export class ModelRole extends AbstractModel{
+    // 角色数据
+    public roleAccount:number;
+    public roleName:string;
+    public roleCreateTime:number;
+    public roleLevel:number;
+    public roleExp:number;
+    public roleGold:number;
+    public roleDiamond:number;
+    public roleVip:number;
+    // 角色英雄数据 英雄ID:英雄结构体HeroVo
+    public heroDic:Record<number,HeroBookCFG> = {}; // 英雄字典
+
     protected init() {
         // 初始化直接加载本地数据
         this.loadLocalRoleData();
